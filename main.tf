ed5a4458b127014980e0ec38f387467fe89dacda
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-19 22:02:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ resource "local_file" "ip" {
 
 #### this variable is being used in workflow to get public ip address of the server
 output "ip_address" {
-   value = "3.81.13.21"
+   value = "3.86.57.252"
 }
