b448dd788ef66d2899bd609cf8cfbe3ffcb2975b
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-19 22:00:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
+### I have not used this block in this code
 resource "local_file" "ip" {
    content  = "3.81.13.21"
     filename = "ip.txt"
 }
 
+#### this variable is being used in workflow to get public ip address of the server
 output "ip_address" {
    value = "3.81.13.21"
 }
